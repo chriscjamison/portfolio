c8638ba9c3152afcfa0d4d4249001bac03fb790f
Griddl <hello@griddl.co>
2020-02-11 17:45:48 -0800

    1.0.7 - Updates to resume

--- assets/php/contact_form.php
+++ assets/php/contact_form.php
@@ -1,16 +1,16 @@
 <?php
-  $to      = 'chris@chriscjamison.com';
+  $to      = "chris@chriscjamison.com";
   $subject = "Message from " . $_POST['full_name'] . ", sent from chriscjamison.com";
   $message = 
-    "From: " . $_POST['full_name'] . "\n\n" . 
-    "Email Address: " . $_POST['email_address'] . "\n" . 
-    "Message: " . "\n" . $_POST['message'];
-  $headers = 'From: no_reply@chriscjamison.com' . "\r\n" .
-             'X-Mailer: PHP/' . phpversion();
+             "From: " . $_POST['full_name'] . "\n\n" . 
+             "Email Address: " . $_POST['email_address'] . "\n" . 
+             "Message: " . "\n" . $_POST['message'];
+  $headers = "From: no_reply@chriscjamison.com" . "\r\n" . 
+             "Reply-To: no_reply@chriscjamison.com" . "\r\n" .
+             "X-Mailer: PHP/" . phpversion();
 
   mail($to, $subject, $message, $headers);
   
-
   $to = $_POST['email_address'];
   $subject = "You sent a message to Chris Jamison";
   $message = "Hey, " . $_POST['full_name'] . ".\n\n" . "Thank you so much for viewing my portfolio and sending your message. \n\n" . 
@@ -20,6 +20,7 @@
              "Thanks you again for reaching out to me. I look forward to meeting you in some form soon. \n\n" .
              "Chris Jamison";
   $headers = "From: no_reply@chriscjamison.com" . "\r\n" . 
+             "Reply-To: no_reply@chriscjamison.com" . "\r\n" .
              "X-Mailer: PHP/" . phpversion(); 
 
   mail($to, $subject, $message, $headers);
